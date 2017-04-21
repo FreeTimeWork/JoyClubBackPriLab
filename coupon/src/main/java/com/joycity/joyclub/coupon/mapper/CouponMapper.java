e847c116bf8c42403059a87b0a5eccbb8a7a8d6f
XYZ <amos.callmexyz@gmail.com>
2017-04-21 18:16:09 +0800

    核销的卡券列表 增加筛选

--- coupon/src/main/java/com/joycity/joyclub/coupon/mapper/CouponMapper.java
+++ coupon/src/main/java/com/joycity/joyclub/coupon/mapper/CouponMapper.java
@@ -3,6 +3,7 @@ package com.joycity.joyclub.coupon.mapper;
 import com.joycity.joyclub.commons.mapper.BaseMapper;
 import com.joycity.joyclub.commons.modal.base.IdName;
 import com.joycity.joyclub.commons.utils.PageUtil;
+import com.joycity.joyclub.coupon.modal.CouponForClient;
 import com.joycity.joyclub.coupon.modal.generated.Coupon;
 import com.joycity.joyclub.coupon.modal.generated.CouponExample;
 import org.apache.ibatis.annotations.Param;
@@ -28,10 +29,27 @@ public interface CouponMapper extends BaseMapper<Coupon, Long, CouponExample> {
                                 @Param("useFlag") Boolean useFlag,
                                 @Param("pageUtil") PageUtil pageUtil);
 
+    List<CouponForClient> selectForFrontByFilter(
+
+            @Param("projectId") Long projectId,
+            @Param("clientId") Long clientId,
+            @Param("cardType") String cardType,
+            @Param("pageUtil") PageUtil pageUtil
+    );  List<CouponForClient> selectForFrontWithoutClientByFilter(
+
+            @Param("projectId") Long projectId,
+            @Param("pageUtil") PageUtil pageUtil
+    );
+
+    List<CouponForClient> selectClientCouponsByFilter(
+            @Param("clientId") Long clientId,
+            @Param("pageUtil") PageUtil pageUtil
+    );
 
     @Update("update coupon set num=num+#{num} where id=#{id}")
     void addNum(@Param("id") Long id, @Param("num") Integer num);
-    @Select("select id,name from coupon ")
+
+    @Select("select id,name from coupon where use_flag=1 and forbid_flag=0 and now() between available_start_time and available_end_time  ")
     List<IdName> getAllSimpleList();
 
 }
