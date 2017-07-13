42c25afe693f023846358b25887452e7c8dc8d72
CallMeXYZ <amos.callmexyz@gmail.com>
2017-07-13 14:14:37 +0800

    【王俊】pom添加quartz

--- system/src/test/java/com/joycity/joyclub/TestApp.java
+++ system/src/test/java/com/joycity/joyclub/TestApp.java
@@ -1,8 +1,11 @@
 package com.joycity.joyclub;
 
-import com.joycity.joyclub.product.service.ProductService;
+import com.alibaba.fastjson.JSON;
+import com.joycity.joyclub.client.modal.Client;
+import com.joycity.joyclub.client.service.KeChuanCrmService;
+import com.joycity.joyclub.commons.constant.Global;
 import com.joycity.joyclub.system.App;
-import com.joycity.joyclub.title_carousel.service.TitleCarouselService;
+import com.joycity.joyclub.test.mapper.TestMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,36 +13,59 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by CallMeXYZ on 2017/6/14.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class)
-@ActiveProfiles(value = "development")
+@ActiveProfiles(Global.PROFILE_DEVELOPMENT)
 public class TestApp {
-//    @Autowired
-//    RechargeService rechargeService;
     @Autowired
-    ProductService productService;
-
+    TestMapper testMapper;
     @Autowired
-    TitleCarouselService titleCarouselService;
-
-//    @Test
-//    public void testRechargeFlow() {
-////        rechargeService.rechargeFlow("17600690739",1,true);
-//
-//    }
-
-//    @Test
-//    public void test(){
-//        ResultData data = productService.getListByProductNameAndStoreName("商品", "哈", new PageUtil(1, 10));
-//        System.out.println(data);
-//    }
+    KeChuanCrmService keChuanCrmService;
 
     @Test
-    public void testTitleCarousel() {
-//        ResultData data = titleCarouselService.getAllTitleCarousel();
-        System.out.println();
+    public void test() {
+        List<Client> list = new ArrayList<>();
+        String prefix = "0500";
+        int size = 400000;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            String code = String.format("%06d", i);
+            Client client = keChuanCrmService.getMemberByTel(prefix + code);
+            if (client != null) {
+                list.add(client);
+            }
+        }
+        System.out.println("访问" + size + "次,获得" + list.size() + "条数据，耗时" + (System.currentTimeMillis() - start));
+        String fileName = "VIPDATA_" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + ".txt";
+        Path savePath = Paths.get("/temp", fileName);
+        // 确保父目录存在
+        try {
+            if (Files.notExists(savePath.getParent())) {
+                Files.createDirectories(savePath.getParent());
+            }
+            // 确保文件不存在
+            if (Files.exists(savePath)) {
+                Files.delete(savePath);
+            }
+            // 创建并写入 写入的文件时一行 在读取的时候readLine即可
+            BufferedWriter writer = Files.newBufferedWriter(savePath);
+            writer.write(JSON.toJSONString(list));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
