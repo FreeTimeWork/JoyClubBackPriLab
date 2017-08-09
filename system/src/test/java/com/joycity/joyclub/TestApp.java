394833b72d22a6c5de1f0dd9f6170d6bca591e59
CallMeXYZ <amos.callmexyz@gmail.com>
2017-08-09 20:49:26 +0800

    删除testApp

--- system/src/test/java/com/joycity/joyclub/TestApp.java
+++ system/src/test/java/com/joycity/joyclub/TestApp.java
@@ -1,7 +1,10 @@
 package com.joycity.joyclub;
 
+import com.joycity.joyclub.mallcoo.service.MallCooService;
 import com.joycity.joyclub.system.App;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,5 +16,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = App.class)
 @ActiveProfiles(value = "development")
 public class TestApp {
+    @Autowired
+    MallCooService mallCooService;
 
+    @Test
+    public void test1() {
+        mallCooService.getCouponInfo(1L, "0500307360");
+    }
+
+    @Test
+    public void test2() {
+        mallCooService.getCoupons(1L, "0500307360");
+    }
 }
